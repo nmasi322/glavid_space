516d85e87c1d5c80cf44297a0a5f502de0a7444b
nmasi322 <edehdivine042@gmail.com>
2021-11-28 00:46:11 +0100

    NextStep Hacks first page is done!

---
+++ index.js
@@ -0,0 +1,54 @@
+var currentSlide = 0;
+const slides = document.querySelectorAll(".slide")
+const dots = document.querySelectorAll('.dot')
+
+const init = (n) => {
+  slides.forEach((slide) => {
+    slide.style.display = "none"
+    dots.forEach((dot) => {
+      dot.classList.remove("active")
+    })
+  })
+  slides[n].style.display = "block"
+  dots[n].classList.add("active")
+}
+document.addEventListener("DOMContentLoaded", init(currentSlide))
+
+const next = () => {
+    currentSlide >= slides.length - 1 ? currentSlide = 0 : currentSlide++
+    init(currentSlide)
+}
+  
+const prev = () => {
+    currentSlide <= 0 ? currentSlide = slides.length - 1 : currentSlide--
+    init(currentSlide)
+}
+  
+document.querySelector(".next").addEventListener('click', next)
+document.querySelector(".prev").addEventListener('click', prev)
+
+setInterval(() => {
+    next()
+}, 3000);
+
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      init(index)
+      currentSlide = i
+    })
+})
+
+
+// menu toggle
+
+let overlay = document.querySelector(".menu_overlay");
+let open = document.querySelector(".fa-bars");
+let close = document.querySelector(".fa-times");
+
+open.addEventListener("click", () => {
+  overlay.style.transform = "translateY(0)";
+});
+
+close.addEventListener("click", () => {
+  overlay.style.transform = "translateY(-110%)";
+});
